928b99332bbaeaf28380754325d4fadf2e5580fe
Oskar Legner <oskarlegner@gmail.com>
2024-10-01 18:45:47 +0200

    Add loops and oop proj files

---
+++ loops/loops.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int h = 5;
+    
+    for (int i = 0; i < h; i++) {
+        cout << "[";
+        cout << i;
+        cout << "] ";
+        cout << "Hello World!\n";
+    }
+
+    int j = 0;
+    while (j < 10) {
+        cout << "[";
+        cout << j;
+        cout << "] ";
+        cout << "Hello While!\n";
+
+        j++;
+    }
+
+    return 0;
+}
+
