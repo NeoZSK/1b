b8eb711a5cecc7abe6bb30763dffb50a4ff7202f
Oskar <oskar.legner@zsk.poznan.pl>
2024-10-23 10:58:30 +0200

    Add rand example

---
+++ rand/rand.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cstdlib> // rand(), srand()
+#include <time.h> // time()
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+	srand(time(0));
+
+	while (true) {
+		cout << (rand() % 6) + 1 << endl;
+		Sleep(1000);
+	}
+
+
+	return 0;
+}
