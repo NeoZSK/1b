27b4a1051ccdda075fa4004f8229ea96cff62b9c
Oskar <oskar.legner@zsk.poznan.pl>
2024-11-06 12:24:20 +0100

    Add functions examples

---
+++ Functions/Functions.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int multiplyByTwo(int x) {
+	return x * 2;
+}
+
+int add(int a, int b) {
+	a = a * 2;
+	return a + b;
+}
+
+
+string merge(string niewiem, string niewiem2) {
+	return niewiem + niewiem2;
+}
+
+int main()
+{
+	string result;
+	result = merge("krzeslo", "aura");
+	cout << result;
+
+	return 0;
+}
+
+
+
+
+
+//int -> 10
+//string -> "aura"
