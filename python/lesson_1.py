c92084ee783e6c7d7eb880b36854e3f429e6b67c
Oskar <oskar.legner@zsk.poznan.pl>
2025-04-02 10:01:50 +0200

    Add python lesson 2

--- python/lesson_1.py
+++ python/lesson_1.py
@@ -3,8 +3,6 @@
 max = int(input("Get value: "))
 
 i = 0
-
-
 while (i < max):
     print(i == 3)
     print(i * '*')
---
+++ python/lesson_2.py
@@ -0,0 +1,136 @@
+# --- variables naming --- 
+something = None
+some_long_name = None
+SOME_CONST = None
+
+Never_Ever_Do_This = None # (ugly!)
+
+# in OOP
+class SomeClassname:
+    _private_variable = None 
+# Note: When using acronyms in CapWords, 
+# capitalize all the letters of the acronym. 
+# Thus HTTPServerError is better than HttpServerError.
+
+# existing:
+__embeded_names__= None
+__name__ == "__main__" # when is a starting file
+
+
+# --- importing modules --- 
+import math
+import keyword as kw
+
+from keyword import kwlist
+
+
+# --- keywords --- 
+# print(kw.kwlist)
+print(kwlist)
+
+
+# --- Continuation of statements --- 
+if (1 == 1) and (1 == 1) and (1 == 1) and (1 == 1) and (1 == 1) and (1 == 1) \
+    and (1 == 1) and (2 == 2):
+    print("Continuation of statements")
+    
+    
+# --- Python Enhancement Proposals ---
+""" PEP8 
+https://peps.python.org/pep-0008/
+
+(that's a multiline comment, btw)
+"""
+    
+    
+# --- Defining functions ---
+def repeat_text(text, times):
+    res = text * times
+    return res
+
+repeat_text("asdf", 5)
+
+
+# --- Flow control ---
+a = 1
+b = 2
+if (a == b):
+    print("equal")
+elif (a > b):
+    print("a > b")    
+else:
+    print("b > a")
+    
+""" note
+`==` VS `is`
+
+Use `==` when comparing values
+Use `is` in OOP in specific cases (is THE SAME object?) - sometimes a little tricky
+"""
+
+
+# --- Loops ---
+sequence = range(0,5)
+print(sequence)
+print(list(sequence))
+
+for i in range(0,5):
+    print(i)
+
+
+# iterate over list
+some_list = [55,22,44, "abs"]
+for item in some_list:
+    print(f"item: {item}")
+
+for idx, item in enumerate(some_list):
+    print(f"item at idx {idx}: {item}")
+
+
+
+# --- (Not so obvious) Operators ---
+x, y = 5, 2
+
+# Division
+print (x / y)
+
+# Floor division (dzielenie całkowite)
+print (x // y)
+
+# Modulus
+print (x % y)
+
+# Exponentiation
+print (x ** y)
+
+
+
+# --- Basic types ---
+
+# bool     
+flag = True
+
+# int      
+x = 42
+
+# float    
+y = 3.14
+
+# str      
+s = "Hello, World!"
+
+# list     
+lst = [1, 2, 3, "apple"]
+
+# tuple    
+tpl = (10, 20, "banana")
+(_, _, b) = tpl
+print(b)
+
+# dict     
+dct = {"name": "Alice", "age": 25}
+print(dct["name"])
+
+# set (unique values)
+st = {1, 2, 3, 4, 2}
+print(st)
