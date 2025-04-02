c6d93776289562de803141e27eef0a25c703c4b0
Oskar <oskar.legner@zsk.poznan.pl>
2025-04-02 08:34:02 +0200

    Clean up repo, add algorithms, create python folder

--- Cpp/OOP/OOP.cpp
+++ Cpp/OOP/OOP.cpp
@@ -18,10 +18,23 @@ public:
     }
 };
 
+
+class Teacher : public Employee {
+    int teachingYears;
+
+    Teacher(string name, string company, int age, int years) : Employee(name, company, age), teachingYears(years) {
+
+    }
+
+
+};
+
 int main()
 {
     Employee e = Employee("Adam", "MS", 27);
     Employee e2 = Employee("Mateusz", "MS", 27);
+
+    Teacher t = Teacher("Aasd", "ZSK", 29, 1);
 
     e.introduceYourself();
     e2.introduceYourself();
---
+++ python/main.py
@@ -0,0 +1,11 @@
+
+
+max = int(input("Get value: "))
+
+i = 0
+
+
+while (i < max):
+    print(i == 3)
+    print(i * '*')
+    i += 1  
